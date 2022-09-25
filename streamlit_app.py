59159f57ac0fe50a2d3c7b11b570e5024553265e
Durga Prasad Kaparapu <114341333+durga-SFev@users.noreply.github.com>
2022-09-25 18:29:36 +0530

    Update streamlit_app.py
    my mom new healthy dinner

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit
 
 streamlit.title('My Mom New Healthy Diner')
 
-streamlit.header('🥣Breakfast Menu')
-streamlit.text('🥗omega 3 & Blueberry oatmeal')
-streamlit.text('🐔kale,spinach & Rocket smoothie')
-streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
-
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣omega 3 & Blueberry oatmeal')
+streamlit.text('🥗kale,spinach & Rocket smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
 
 
