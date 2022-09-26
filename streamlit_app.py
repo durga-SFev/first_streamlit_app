21b8f3885517506d289bb47181f90b51fa33c6a1
Durga Prasad Kaparapu <114341333+durga-SFev@users.noreply.github.com>
2022-09-26 20:20:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,23 +32,25 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice API responce
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-#streamlit.write('The user entered ', fruit_choice)
-if not fruit_choice:
-  streamlit.error("please select a fruit to get informtion.")
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get a Information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+  
+#streamlit.write('The user entered ', fruit_choice)
 
-except URLError as e:
-  streamlit.error()
 #import requests
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 
 # write your own comment -what does the next line do? 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
+    streamlit.dataframe(fruityvice_normalized)
+  
+except URLError as e:
+  streamlit.error()
 
 #lesson-12
 #dont run anything past here while we troubleshoot
@@ -67,7 +69,3 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
-
-
-
